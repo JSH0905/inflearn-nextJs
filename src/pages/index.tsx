0e13213ef00b6d756c6927890f3a8425e527881a
Jeong Seonghun <2000tjdgns@naver.com>
2025-01-03 13:49:05 +0900

    feat:SSR -> SSG로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,26 @@
 import SearchableLayout from "@/components/searchable-layout";
 import style from "./index.module.css";
-import { ReactNode, useEffect } from "react";
-import books from "@/mock/books.json"; // @ = src 폴더를 가르킴
+import { ReactNode } from "react";
 import BookItem from "@/components/book-item";
-import { GetServerSideProps, InferGetServerSidePropsType } from "next";
+import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
 
 // SSR 하는함수
-export const getServerSideProps = async () => {
+export const getStaticProps = async () => {
+  console.log("인덱스 페이지");
+
   // 컴포넌트보다 먼저 실행되어서, 컴포넌트에 필요한 데이터를 불러오는 함수
 
-  const allBooks = await fetchBooks();
-  const recoBooks = await fetchRandomBooks();
+  // 1. 기본 코드
+  // const allBooks = await fetchBooks();
+  // const recoBooks = await fetchRandomBooks();
+
+  // 2. 개선 코드
+  const [allBooks, recoBooks] = await Promise.all([
+    fetchBooks(),
+    fetchRandomBooks(),
+  ]);
 
   return {
     props: {
@@ -25,7 +33,7 @@ export const getServerSideProps = async () => {
 export default function Home({
   allBooks,
   recoBooks,
-}: InferGetServerSidePropsType<typeof getServerSideProps>) {
+}: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
     <div className={style.container}>
       <section>
