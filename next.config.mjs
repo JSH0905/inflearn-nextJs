eec9dada2785f0fede582c11db06b9e67f01ad50
Jeong Seonghun <2000tjdgns@naver.com>
2025-01-03 12:35:43 +0900

    feat:Page Router SSR 구현

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false, // true일 경우 개발모드로 실습시 불편함
 };
 
 export default nextConfig;
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,8 +1,10 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
+// 모든 페이지에 공통적으로 적용되어야하는 Next 앱의 HTML 컴포넌트
+
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="kr">
       <Head />
       <body>
         <Main />
