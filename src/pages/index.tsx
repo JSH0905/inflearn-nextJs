8d8a97db11436bf4399868c628d28ae0e21a1516
Jeong Seonghun <2000tjdgns@naver.com>
2025-01-04 22:27:50 +0900

    feat:SEO 설정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -5,13 +5,9 @@ import BookItem from "@/components/book-item";
 import { InferGetStaticPropsType } from "next";
 import fetchBooks from "@/lib/fetch-books";
 import fetchRandomBooks from "@/lib/fetch-random-books";
+import Head from "next/head";
 
-// 웬만해서 코드 짤땐 ISR 방식을 추천함.
-// SSR 하는함수
 export const getStaticProps = async () => {
-  console.log("인덱스 페이지");
-
-  // 2. 개선 코드
   const [allBooks, recoBooks] = await Promise.all([
     fetchBooks(),
     fetchRandomBooks(),
@@ -22,7 +18,6 @@ export const getStaticProps = async () => {
       allBooks,
       recoBooks,
     },
-    // revalidate: 3, // ISR을 사용하기 위한 props 설정 , 일반적인 ISR 방식
   };
 };
 
@@ -31,20 +26,31 @@ export default function Home({
   recoBooks,
 }: InferGetStaticPropsType<typeof getStaticProps>) {
   return (
-    <div className={style.container}>
-      <section>
-        <h3>지금 추천하는 도서</h3>
-        {recoBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-      <section>
-        <h3>등록된 모든 도서</h3>
-        {allBooks.map((book) => (
-          <BookItem key={book.id} {...book} />
-        ))}
-      </section>
-    </div>
+    <>
+      <Head>
+        <title>한입북스</title>
+        <meta property="og:image" content="/thumbnail.png" />
+        <meta property="og:title" content="한입북스" />
+        <meta
+          property="og:description"
+          content="한입 북스에 등록된 도서들을 만나보세요"
+        />
+      </Head>
+      <div className={style.container}>
+        <section>
+          <h3>지금 추천하는 도서</h3>
+          {recoBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+        <section>
+          <h3>등록된 모든 도서</h3>
+          {allBooks.map((book) => (
+            <BookItem key={book.id} {...book} />
+          ))}
+        </section>
+      </div>
+    </>
   );
 }
 
